2c21825b1c321cbc0211c788f1f16a7c15244574
Ed Pase <ed.pase@yahoo.com>
2018-11-04 03:56:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 puts "Welcome to Tic Tac Toe"
 def display_board
-puts"    |   |   "
-puts" -----------"
-puts"    |   |   "
-puts" -----------"
-puts"    |   |   "
+  puts"    |   |   "
+  puts" -----------"
+  puts"    |   |   "
+  puts" -----------"
+  puts"    |   |   "
 end
 display_board 
